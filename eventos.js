cd1305563e11b6bc3b94755a4f7c19e4f7f06adc
crisvaliente <valientecristian536@gmail.com>
2023-08-06 06:43:51 -0300

    Update
    Final

--- eventos.js
+++ eventos.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Seleccionar el div por su id o clase, por ejemplo, con un id "mi-div":
-    const miDiv = document.getElementById("midiv"); // Reemplaza "mi-div" con el id de tu div, si lo tienes con un id.
     
-    // Agregar el manejador de eventos para el clic en el div:
+    const miDiv = document.getElementById("midiv"); 
+    
+    
     miDiv.addEventListener("click", function() {
         alert("Hola! Soy el div");
     });
