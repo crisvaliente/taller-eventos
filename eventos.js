4511be5d733caad38be03a861fe641181def03a4
crisvaliente <valientecristian536@gmail.com>
2023-08-06 07:25:21 -0300

    Solucion
    Se solucionó las dobles alertas

--- eventos.js
+++ eventos.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const miDiv = document.getElementById("midiv"); 
-    
-    
-    miDiv.addEventListener("click", function() {
-        alert("Hola! Soy el div");
-    });
+const miBoton = document.getElementById("miboton");
+const miDiv = document.getElementById("midiv");
+
+miBoton.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+    alert("Hola!");
+});
+
+miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
 });
